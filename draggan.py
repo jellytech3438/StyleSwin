fixed: attention mask / add: gui functions select image, mask, points

draggan.py
@@ -10,6 +10,8 @@ import lpips
 from einops import rearrange
 import torch.nn.functional as FUNC
 from dift_sd import SDFeaturizer
+from PIL import Image
+from torchvision.transforms import PILToTensor
 
 
 def linear(feature, p0, p1, d, axis=0):
@@ -35,24 +37,34 @@ def bilinear(feature, qi, d):
     return fx
 
 
-def motion_supervision(F0, F, pi, ti, r1=3, M=None, lambda_mask=20):
+def motion_supervision(F0, F, pi, ti, layer_index, r1=3, M=None, lambda_mask=20):
     # print("M F size:", F.transpose(-1, -2).size())
     # print("M F0 size:", F0.transpose(-1, -2).size())
 
-    # layer1: [1,1024,512] -> [1,512,1024] -> [1,512,32,32] -> [1,512,256,256]
-    # F = F.transpose(-1,-2).reshape(1,512,32,32)
-    # F0 = F0.transpose(-1,-2).reshape(1,512,32,32)
+    if layer_index == 3:
+        # layer1: [1,1024,512] -> [1,512,1024] -> [1,512,32,32] -> [1,512,256,256]
+        F = F.transpose(-1,-2).reshape(1,512,32,32)
+        F0 = F0.transpose(-1,-2).reshape(1,512,32,32)
+        F = functional.interpolate(F, [256, 256], mode="bilinear")
+        F0 = functional.interpolate(F0, [256, 256], mode="bilinear")
+    elif layer_index == 4:
+        # layer 4: [1,4096,512] -> [1,512,4096] -> [1,512,64,64] -> [1,512,256,256]
+        F = F.transpose(-1,-2).reshape(1,512,64,64)
+        F0 = F0.transpose(-1,-2).reshape(1,512,64,64)
+        
+        F = functional.interpolate(F, [256, 256], mode="bilinear")
+        F0 = functional.interpolate(F0, [256, 256], mode="bilinear")
+    elif layer_index == 5:
+        # layer2: [1,16384,256] -> [1,256,16384] -> [1,256,128,128] -> [1,256,256,256]
+        F = F.transpose(-1,-2).reshape(1,256,128,128)
+        F0 = F0.transpose(-1,-2).reshape(1,256,128,128)
 
-    # layer2: [1,16384,256] -> [1,256,16384] -> [1,256,128,128] -> [1,256,256,256]
-    # F = F.transpose(-1,-2).reshape(1,256,128,128)
-    # F0 = F0.transpose(-1,-2).reshape(1,256,128,128)
-    #
-    # F = functional.interpolate(F, [256, 256], mode="bilinear")
-    # F0 = functional.interpolate(F0, [256, 256], mode="bilinear")
-
-    # layer3: [1,65536,128] -> [1,128,65536] -> [1,128,256,256]
-    F = F.transpose(-1, -2).reshape(1, 128, 256, 256)
-    F0 = F0.transpose(-1, -2).reshape(1, 128, 256, 256)
+        F = functional.interpolate(F, [256, 256], mode="bilinear")
+        F0 = functional.interpolate(F0, [256, 256], mode="bilinear")
+    elif layer_index >= 6:
+        # layer3: [1,65536,128] -> [1,128,65536] -> [1,128,256,256]
+        F = F.transpose(-1, -2).reshape(1, 128, 256, 256)
+        F0 = F0.transpose(-1, -2).reshape(1, 128, 256, 256)
 
     dw, dh = ti[0] - pi[0], ti[1] - pi[1]
     norm = math.sqrt(dw**2 + dh**2)
@@ -108,24 +120,35 @@ def check_handle_reach_target(handle_points,
 
 
 @torch.no_grad()
-def point_tracking(F0, F, pi, p0, r2=12):
+def point_tracking(F0, F, pi, p0, layer_index, r2=12):
     # print("P F size:", F.transpose(-1, -2).size())
     # print("P F0 size:", F0.transpose(-1, -2).size())
 
-    # layer1: [1,1024,512] -> [1,512,1024] -> [1,512,32,32] -> [1,512,256,256]
-    # F = F.transpose(-1,-2).reshape(1,512,32,32)
-    # F0 = F0.transpose(-1,-2).reshape(1,512,32,32)
+    if layer_index == 3:
+        # layer 3: [1,1024,512] -> [1,512,1024] -> [1,512,32,32] -> [1,512,256,256]
+        F = F.transpose(-1, -2).reshape(1, 512, 32, 32)
+        F0 = F0.transpose(-1, -2).reshape(1, 512, 32, 32)
+        
+        F = functional.interpolate(F, [256, 256], mode="bilinear")
+        F0 = functional.interpolate(F0, [256, 256], mode="bilinear")
+    elif layer_index == 4:
+        # layer 4: [1, 4096, 512] -> [1, 512, 4096] -> [1, 512, 64, 64] -> [1, 512, 256, 256]
+        F = F.transpose(-1, -2).reshape(1, 512, 64, 64)
+        F0 = F0.transpose(-1, -2).reshape(1, 512, 64, 64)
+        
+        F = functional.interpolate(F, [256, 256], mode="bilinear")
+        F0 = functional.interpolate(F0, [256, 256], mode="bilinear")
+    elif layer_index == 5:
+        # layer 5: [1, 16384, 256] -> [1, 256, 16384] -> [1, 256, 128, 128] -> [1, 256, 256, 256]
+        F = F.transpose(-1, -2).reshape(1, 256, 128, 128)
+        F0 = F0.transpose(-1, -2).reshape(1, 256, 128, 128)
 
-    # layer2: [1,16384,256] -> [1,256,16384] -> [1,256,128,128] -> [1,256,256,256]
-    # F = F.transpose(-1,-2).reshape(1,256,128,128)
-    # F0 = F0.transpose(-1,-2).reshape(1,256,128,128)
-    #
-    # F = functional.interpolate(F, [256, 256], mode="bilinear")
-    # F0 = functional.interpolate(F0, [256, 256], mode="bilinear")
-
-    # layer3: [1,65536,128] -> [1,128,65536] -> [1,128,256,256]
-    F = F.transpose(-1, -2).reshape(1, 128, 256, 256)
-    F0 = F0.transpose(-1, -2).reshape(1, 128, 256, 256)
+        F = functional.interpolate(F, [256, 256], mode="bilinear")
+        F0 = functional.interpolate(F0, [256, 256], mode="bilinear")
+    elif layer_index >= 6:
+        # layer 6 7: [1, 65536, 128] -> [1, 128, 65536] -> [1, 128, 256, 256]
+        F = F.transpose(-1, -2).reshape(1, 128, 256, 256)
+        F0 = F0.transpose(-1, -2).reshape(1, 128, 256, 256)
 
     x = (max(0, pi[0] - r2), min(256, pi[0] + r2))
     y = (max(0, pi[1] - r2), min(256, pi[1] + r2))
@@ -144,11 +167,14 @@ def requires_grad(model, flag=True):
 
 
 class DragGAN():
-    def __init__(self, device, layer_index=3):
+    def __init__(self, device, train_fixed_layer=6, layer_index=7):
+        # layer index = 3~7 becuase of pt and ms interpolation
+        # train fixed index = 0~7
         self.generator = Generator(256, 512, 8).to(device)
         requires_grad(self.generator, False)
         self._device = device
         self.layer_index = layer_index
+        self.train_fixed_index = train_fixed_layer
         self.latent = None
         self.F0 = None
         self.optimizer = None
@@ -206,20 +232,20 @@ class DragGAN():
 
     @torch.no_grad()
     def generate_image(self, seed):
+        # np random
         # z = torch.from_numpy(
         #     np.random.RandomState(seed).randn(1, 512).astype(np.float32)
         # ).to(self._device)
+
+        # torch random
         gen = torch.Generator()
         gen = gen.manual_seed(seed)
         z = torch.randn(1, 512, generator=gen).to(self._device)
-        print("z: ", z.size())
+
         image, self.latent, self.F0, _ = self.generator(
             z, return_latents=True, return_features=True
         )
-        # v2
-        # imaeg = image[0]
-        # v1
-        image, self.F0 = image[0], self.F0[self.layer_index*2+1].detach()
+        image, self.F0 = image[0], self.F0[self.layer_index].detach()
         # OG
         # image, self.F0 = image[0], self.F0[self.layer_index+1].detach()
         image = (1/(2*2.8)) * \
@@ -258,69 +284,56 @@ class DragGAN():
         plt.imshow(img)
         plt.savefig("./features/{}.png".format(steps))
 
-    # overlay = [0, 1]
     # select_layer = [6, 7]
     # feature_channel = range(128)
-    def step(self, points, mask, step, overlay=None, select_layer=6, feature_channel=4, visiualize_attention=False):
+    def step(self, points, mask, step, feature_channel=4, visiualize_attention=False):
         if self.optimizer is None:
             len_pts = (len(points) // 2) * 2
             if len_pts == 0:
                 print('Select at least one pair of points')
                 return False, None
-            self.trainable = self.latent[:, :self.layer_index *
-                                         2-1, :].detach().requires_grad_(True)
-            self.fixed = self.latent[:, self.layer_index *
-                                     2-1:, :].detach().requires_grad_(False)
-            # self.trainable = self.latent[:, :self.layer_index, :].detach().requires_grad_(True)
-            # self.fixed = self.latent[:, self.layer_index:, :].detach().requires_grad_(False)
+            self.trainable = self.latent[:, :self.train_fixed_index, :].detach().requires_grad_(True)
+            self.fixed = self.latent[:, self.train_fixed_index:, :].detach().requires_grad_(False)
 
             self.optimizer = torch.optim.Adam([self.trainable], lr=2e-3)
             points = points[:len_pts]
             self.p0 = points[::2]
             print("p0: ", self.p0)
+            
         self.optimizer.zero_grad()
         trainable_fixed = torch.cat([self.trainable, self.fixed], dim=1)
+        
+
+        # init features and image for point tracking
         image, _, store_features, attentions = self.generator(
             trainable_fixed,  # this is why noise = [tensor[]]
             input_is_latent=True,
             return_features=True,
             return_attention=visiualize_attention,
-            mask1=torch.tensor(mask, dtype=torch.float).to("cuda")
+            mask1=torch.tensor(1-mask, dtype=torch.float).to("cuda")
         )
 
+        # v1 and OG
+        features = store_features[self.layer_index]
         # v2
-        # fetures = store_features
-        # v1
-        features = store_features[self.layer_index*2+1]
+        # features = store_features
+        
         new_points = points
-        # OG
-        # features = features[self.layer_index+1]
-
-        # pt start
-        # init features and image for point tracking
-        # image, _, features = self.generator(
-        #     trainable_fixed,
-        #     input_is_latent=True,
-        #     return_features=True
-        # )
-        # v2
-        # features = features
-        # v1
-        # features = features[self.layer_index*2+1]
-        # OG
-        # features = features[self.layer_index+1]
 
         store_image = (1/(2*2.8)) * \
             image[0].detach().cpu().permute(1, 2, 0).numpy() + 0.5
         image = store_image.clip(0, 1).reshape(-1)
+
+        # pt start
         for i in range(len(self.p0)):
             new_points[2*i] = point_tracking(self.F0,
-                                             features, points[2*i], self.p0[i])
+                                             features, points[2*i], self.p0[i], self.layer_index)
 
         loss = 0
+        # ms start
         for i in range(len(self.p0)):
             msloss, og_loss, mask_loss = motion_supervision((self.F0),
-                                                            features, points[2*i], points[2*i+1], M=mask, lambda_mask=self.lambda_mask)
+                                                            features, points[2*i], points[2*i+1], self.layer_index ,M=mask, lambda_mask=self.lambda_mask)
             loss += msloss
             self.og_loss_recod.append(og_loss)
             self.mask_loss_record.append(mask_loss)
@@ -374,86 +387,6 @@ class DragGAN():
                 plt.savefig(f"attention\\{step}.png")
             plt.close()
 
-        # calculate with selection layer mask if user choose
-        # start selection overlay
-        # there are eight layers in features layer of six and seven
-        # overlay:
-        #     choose from 1~8 to determine the thresholding one
-        # selection:
-        #     0 means lower and 1 means upper overlay
-
-        if overlay is not None:
-            # draw mask parameters
-            lower = 0
-            upper = 1
-            mask_area = 64
-            im_w, im_h = 256, 256
-
-            # feature map heat-map extracting
-            d = store_features[select_layer].size(dim=1)
-            d2 = store_features[select_layer].size(dim=2)
-            fts = store_features[select_layer].transpose(-1, -2).reshape(
-                1, d2, int(math.sqrt(d)), int(math.sqrt(d)))
-
-            img = fts[0]
-            temp = np.zeros((im_w, im_h))
-
-            # noramlization
-            temp = img[feature_channel] - img[feature_channel].min()
-            temp = temp / temp.max()
-            temp = temp * 255
-            temp = temp.data.cpu().numpy().astype(np.uint8)
-
-            blur = cv2.GaussianBlur(temp, (5, 5), 0)
-            mask = np.ones([256, 256], np.uint8)
-
-            # T = int(ret3)
-            # P = temp[points[0][1], points[0][0]]
-
-            # draw mask according to thresholding and selection layer
-            # this mask only masks a rectangle over the start points
-
-            for p in range(len(points)):
-                # only select start points
-                if p % 2 == 1:
-                    continue
-                x = points[p][0]
-                y = points[p][1]
-                x_start, x_end = max(
-                    0, x - mask_area), min(im_w, x + mask_area)
-                y_start, y_end = max(
-                    0, y - mask_area), min(im_h, y + mask_area)
-                area_blur = blur[y_start:y_end, x_start:x_end]
-                ret, thres = cv2.threshold(
-                    area_blur, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-                thres = thres / 255
-
-                if overlay == upper:
-                    mask[y_start:y_end, x_start:x_end] = 1 - thres
-                elif overlay == lower:
-                    mask[y_start:y_end, x_start:x_end] = thres
-
-            # for p in range(len(points)):
-            #     # only select start points
-            #     if p % 2 == 1:
-            #         continue
-            #     x = points[p][0]
-            #     y = points[p][1]
-            #     x_start, x_end = max(0, x - mask_area), min(im_w, x + mask_area)
-            #     y_start, y_end = max(0, y - mask_area), min(im_h, y + mask_area)
-            #     print(x,y,x_start,x_end,y_start,y_end)
-            #     for x in range(x_start, x_end):
-            #         for y in range(y_start, y_end):
-            #             if selection == upper:
-            #                 if img[y,x] < 0:
-            #                     mask_upper[y,x] = 0
-            #                 else:
-            #                     mask_upper[y,x] = 1 - img[y,x]
-            #             else:
-            #                 if img[y,x] > 0:
-            #                     mask_lower[y,x] = 0
-            #                 else:
-            #                     mask_lower[y,x] = img[y,x] + 1
 
         print(loss)
         loss.backward()
@@ -471,18 +404,16 @@ class DragGAN():
         if check_handle_reach_target(pi, ti):
             status = False
 
-        # return bool means keep iterating or not
+        # status means keep iterating or not
         return status, (points, image), store_features, store_image
 
     def preprocess_image(self, image):
         image = image * 255
         image = image.astype(np.uint8)
-        for i in range(len(image), -1, -1):
-            if i % 4 == 3:
-                image = np.delete(image, i)
-        print(image.shape)
-        image = image.reshape((256, 256, 3))
-        image = torch.from_numpy(image).float() / 127.5 - 1  # [-1, 1]
+        image = Image.fromarray(image.reshape((256, 256, 4)))
+        image = image.convert('RGB')
+        image = torch.from_numpy(
+            np.array(image)).float() / 127.5 - 1  # [-1, 1]
         image = rearrange(image, "h w c -> 1 c h w")
         image = image.to(self._device)
         return image
@@ -519,11 +450,25 @@ class DragGAN():
 
         dift = SDFeaturizer('stabilityai/stable-diffusion-2-1')
 
-        source_image_tensor = self.preprocess_image(np.array(origin_image))
-        dragged_image_tensor = self.preprocess_image(np.array(raw_data))
-        print(source_image_tensor.shape, dragged_image_tensor.shape)
+        # source_image_tensor = self.preprocess_image(np.array(origin_image))
+        # dragged_image_tensor = self.preprocess_image(np.array(raw_data))
+        # print(source_image_tensor.shape, dragged_image_tensor.shape)
 
-        _, C, H, W = source_image_tensor.shape
+        source_image = np.array(origin_image) * 255
+        source_image = source_image.astype(np.uint8)
+        source_image = Image.fromarray(source_image.reshape((256, 256, 4)))
+        source_image = source_image.convert('RGB')
+        source_image_tensor = (PILToTensor()(
+            source_image) / 255.0 - 0.5) * 2
+
+        dragged_image = np.array(raw_data) * 255
+        dragged_image = dragged_image.astype(np.uint8)
+        dragged_image = Image.fromarray(dragged_image.reshape((256, 256, 4)))
+        dragged_image = dragged_image.convert('RGB')
+        dragged_image_tensor = (PILToTensor()(
+            dragged_image) / 255.0 - 0.5) * 2
+
+        _, H, W = source_image_tensor.shape
         ft_source = dift.forward(source_image_tensor,
                                  prompt='',
                                  t=261,

gui.py
@@ -25,12 +25,14 @@ image_pixels = image_height * image_width
 raw_data_size = image_width * image_height * rgba_channel
 raw_data = array('f', [1] * raw_data_size)
 origin_image = array('f', [1] * raw_data_size)
+store_image = array('f', [1] * raw_data_size)
 device = "cuda"
+store_feat_step = 5
 
 model = DragGAN(device)
 
 dpg.create_context()
-dpg.create_viewport(title='DragSwin', width=1600, height=1600)
+dpg.create_viewport(title='mm-final', width=1600, height=1600)
 
 with dpg.texture_registry(show=False):
     dpg.add_raw_texture(
@@ -44,6 +46,14 @@ def update_image(new_image):
     for i in range(0, image_pixels):
         rd_base, im_base = i * rgba_channel, i * rgb_channel
         raw_data[rd_base:rd_base + rgb_channel] = array(
+            'f', new_image[im_base:im_base + rgb_channel]
+        )
+
+
+def update_save_image(new_image):
+    for i in range(0, image_pixels):
+        rd_base, im_base = i * rgba_channel, i * rgb_channel
+        store_image[rd_base:rd_base + rgb_channel] = array(
             'f', new_image[im_base:im_base + rgb_channel]
         )
 
@@ -73,16 +83,10 @@ def set_lambda_mask(sender, app_data):
 
 
 def dragging_thread():
-    global points, steps, dragging, mask
+    global points, steps, dragging, mask, plot_loss, store_image
     while (dragging):
-        # if steps < 2:
-        #     status, ret, sfeatures, simage = model.step(
-        #         points, mask, steps, visiualize_attention=True)
-        # else:
-        #     status, ret, sfeatures, simage = model.step(
-        #         points, mask, steps, visiualize_attention=False)
         status, ret, sfeatures, simage = model.step(
-            points, mask, steps, visiualize_attention=True)
+            points, mask, steps, visiualize_attention=dpg.get_value("vis attention"))
         if status:
             points, image = ret
         else:
@@ -90,18 +94,20 @@ def dragging_thread():
             print("reach target points")
             break
         update_image(image)
+        update_save_image(image)
         for i in range(len(points)):
             draw_point(*points[i], point_color[i % 2])
         steps += 1
 
-        # store features map into features folder every 5 steps
-        # if steps % 5 == 0:
-        #     model.store_feature(steps, simage, sfeatures)
+        # store features map into features folder every n steps
+        if dpg.get_value("store feature") and steps % store_feat_step == 0:
+            model.store_feature(steps, simage, sfeatures)
         dpg.set_value('steps', f'steps: {steps}')
 
     # plot loss
-    model.plot_loss()
-    print("finish loss plot saving")
+    if dpg.get_value("plot loss"):
+        model.plot_loss()
+        print("saves loss ploting")
 
     # call calculate score function after dragging
     lpips = model.lpip_score(origin_image, raw_data)
@@ -184,6 +190,12 @@ with dpg.window(
     dpg.add_button(label="generate", pos=(70, 120), callback=generate_image)
     dpg.add_text('image', pos=(5, 140))
     dpg.add_button(label="select image", pos=(70, 140), callback=None)
+    dpg.add_checkbox(label='store feature', tag='store feature',
+                     pos=(70, 160), default_value=False)
+    dpg.add_checkbox(label='plot loss', tag='plot loss',
+                     pos=(70, 180), default_value=False)
+    dpg.add_checkbox(label='vis attention', tag='vis attention',
+                     pos=(70, 200), default_value=False)
 
 posy += height + 2
 with dpg.window(
@@ -212,7 +224,12 @@ with dpg.window(
 
     def reset_mask():
         global mask
-        mask = np.fill(1)
+        for y in range(256):
+            for x in range(256):
+                if mask[y, x] == 0:
+                    mask[y, x] = 1
+                    raw_data[256*y*rgba_channel + x*rgba_channel] = 0.
+        # print(np.count_nonzero(mask))
 
     def print_mask():
         global mask
@@ -249,8 +266,10 @@ posy += height + 2
 
 
 def save_image(sender, app_data):
-    global raw_data, image_width, image_height
-    temp = np.array(raw_data) * 255
+    global store_image, raw_data, origin_image, image_width, image_height
+    temp = np.array(origin_image) * 255
+    # temp = np.array(store_image) * 255
+    # temp = np.array(raw_data) * 255
     im = Image.fromarray(temp.astype(
         np.uint8).reshape((height, width, 4)))
     im.save("imgs/"+dpg.get_value("save image name")+'.png',)
@@ -264,6 +283,12 @@ def save_points(sender, app_data):
         return 0
     np.save("points/latest_points", np.array(points))
     print("save points successfully")
+
+
+def save_mask(sender, app_data):
+    global mask
+    np.save("mask/latest_mask", np.array(mask))
+    print("save mask successfully")
 
 
 with dpg.window(
@@ -308,6 +333,35 @@ with dpg.window(
     dpg.add_button(
         label="load points", width=100, callback=lambda: dpg.show_item("points selector"),
         pos=(70, 80),
+    )
+
+    dpg.add_text('mask', pos=(5, 100))
+    dpg.add_button(
+        label="save mask", width=100, pos=(70, 100), callback=save_mask
+    )
+
+    def select_mask(sender, app_data):
+        selections = app_data['selections']
+        if selections:
+            for fn in selections:
+                global mask
+                select_mask = np.load(selections[fn]).tolist()
+                mask = np.array(select_mask)
+                print(mask.shape)
+                print("load mask successfully")
+                break
+
+    def cancel_mask(sender, app_data):
+        ...
+
+    with dpg.file_dialog(
+        directory_selector=False, show=False, callback=select_mask, id='mask selector',
+        cancel_callback=cancel_pt, width=700, height=400
+    ):
+        dpg.add_file_extension('.*')
+    dpg.add_button(
+        label="load mask", width=100, callback=lambda: dpg.show_item("mask selector"),
+        pos=(70, 120),
     )
 
 
@@ -427,10 +481,4 @@ def update_series():
 dpg.setup_dearpygui()
 dpg.show_viewport()
 dpg.start_dearpygui()
-# while dpg.is_dearpygui_running():
-#     dpg.set_item_width("Image Win", image_size)
-#     dpg.set_item_height("Image Win", image_size)
-#
-#     dpg.render_dearpygui_frame()
-
 dpg.destroy_context()

models/generator.py
@@ -110,72 +110,6 @@ def window_reverse(windows, window_size, H, W):
     return x
 
 
-# RoPE functions
-
-def init_t_xy(end_x: int, end_y: int, zero_center=False):
-    t = torch.arange(end_x * end_y, dtype=torch.float32)
-    t_x = (t % end_x).float()
-    t_y = torch.div(t, end_x, rounding_mode='floor').float()
-
-    return t_x, t_y
-
-
-def init_random_2d_freqs(head_dim: int, num_heads: int, theta: float = 10.0, rotate: bool = True):
-    freqs_x = []
-    freqs_y = []
-    theta = theta
-    mag = 1 / (theta ** (torch.arange(0, head_dim, 4)
-               [: (head_dim // 4)].float() / head_dim))
-    for i in range(num_heads):
-        angles = torch.rand(1) * 2 * torch.pi if rotate else torch.zeros(1)
-        fx = torch.cat([mag * torch.cos(angles), mag *
-                       torch.cos(torch.pi/2 + angles)], dim=-1)
-        fy = torch.cat([mag * torch.sin(angles), mag *
-                       torch.sin(torch.pi/2 + angles)], dim=-1)
-        freqs_x.append(fx)
-        freqs_y.append(fy)
-    freqs_x = torch.stack(freqs_x, dim=0)
-    freqs_y = torch.stack(freqs_y, dim=0)
-    freqs = torch.stack([freqs_x, freqs_y], dim=0)
-    return freqs
-
-
-def compute_cis(freqs, t_x, t_y):
-    N = t_x.shape[0]
-    # No float 16 for this range
-    with torch.cuda.amp.autocast(enabled=False):
-        freqs_cis = []
-        freqs_x = (t_x.unsqueeze(-1) @ freqs[0].unsqueeze(-2))
-        freqs_y = (t_y.unsqueeze(-1) @ freqs[1].unsqueeze(-2))
-        freqs_cis.append(torch.polar(
-            torch.ones_like(freqs_x), freqs_x + freqs_y))
-
-    return torch.cat(freqs_cis, dim=-1)
-
-
-def reshape_for_broadcast(freqs_cis: torch.Tensor, x: torch.Tensor):
-    ndim = x.ndim
-    assert 0 <= 1 < ndim
-    # assert freqs_cis.shape == (x.shape[-2], x.shape[-1])
-    if freqs_cis.shape == (x.shape[-2], x.shape[-1]):
-        shape = [d if i >= ndim-2 else 1 for i, d in enumerate(x.shape)]
-    elif freqs_cis.shape == (x.shape[-3], x.shape[-2], x.shape[-1]):
-        shape = [d if i >= ndim-3 else 1 for i, d in enumerate(x.shape)]
-
-
-def apply_rotary_emb(
-    xq: torch.Tensor,
-    xk: torch.Tensor,
-    freqs_cis: torch.Tensor,
-) -> Tuple[torch.Tensor, torch.Tensor]:
-    xq_ = torch.view_as_complex(xq.float().reshape(*xq.shape[:-1], -1, 2))
-    xk_ = torch.view_as_complex(xk.float().reshape(*xk.shape[:-1], -1, 2))
-    freqs_cis = reshape_for_broadcast(freqs_cis, xq_)
-    xq_out = torch.view_as_real(xq_ * freqs_cis).flatten(3)
-    xk_out = torch.view_as_real(xk_ * freqs_cis).flatten(3)
-    return xq_out.type_as(xq).to(xq.device), xk_out.type_as(xk).to(xk.device)
-
-
 class WindowAttention(nn.Module):
     r""" Window based multi-head self attention (W-MSA) module with relative position bias.
     It supports both of shifted and non-shifted window.
@@ -194,48 +128,10 @@ class WindowAttention(nn.Module):
         super().__init__()
         self.dim = dim
         self.window_size = window_size  # Wh, Ww
-        # v2
-        # self.pretrained_window_size = pretrained_window_size
-        ###
         self.num_heads = num_heads
         head_dim = dim // num_heads
         self.head_dim = head_dim
         self.scale = qk_scale or head_dim ** -0.5
-
-        # v2
-        # self.logit_scale = nn.Parameter(
-        #     torch.log(10 * torch.ones((num_heads, 1, 1))), requires_grad=True)
-        ###
-
-        # v2
-        # mlp to generate continuous relative position bias
-        # self.cpb_mlp = nn.Sequential(nn.Linear(2, 512, bias=True),
-        #                              nn.ReLU(inplace=True),
-        #                              nn.Linear(512, num_heads, bias=False))
-        #
-        # # get relative_coords_table
-        # relative_coords_h = torch.arange(
-        #     -(self.window_size[0] - 1), self.window_size[0], dtype=torch.float32)
-        # relative_coords_w = torch.arange(
-        #     -(self.window_size[1] - 1), self.window_size[1], dtype=torch.float32)
-        # relative_coords_table = torch.stack(
-        #     torch.meshgrid([relative_coords_h,
-        #                     # 1, 2*Wh-1, 2*Ww-1, 2
-        #                     relative_coords_w])).permute(1, 2, 0).contiguous().unsqueeze(0)
-        # if pretrained_window_size[0] > 0:
-        #     relative_coords_table[:, :, :,
-        #                           0] /= (pretrained_window_size[0] - 1)
-        #     relative_coords_table[:, :, :,
-        #                           1] /= (pretrained_window_size[1] - 1)
-        # else:
-        #     relative_coords_table[:, :, :, 0] /= (self.window_size[0] - 1)
-        #     relative_coords_table[:, :, :, 1] /= (self.window_size[1] - 1)
-        # relative_coords_table *= 8  # normalize to -8, 8
-        # relative_coords_table = torch.sign(relative_coords_table) * torch.log2(
-        #     torch.abs(relative_coords_table) + 1.0) / np.log2(8)
-        #
-        # self.register_buffer("relative_coords_table", relative_coords_table)
-        ###
 
         # define a parameter table of relative position bias
         # in swin-t there's no use
@@ -265,11 +161,6 @@ class WindowAttention(nn.Module):
 
         self.softmax = nn.Softmax(dim=-1)
 
-        # v2
-        # self.proj = nn.Linear(dim, dim)
-        # self.proj_drop = nn.Dropout(proj_drop)
-        ###
-
     def forward(self, q, k, v, mask=None):
         """
         Args:
@@ -288,40 +179,17 @@ class WindowAttention(nn.Module):
 
         q = q * self.scale
 
-        # v1
         attn = (q @ k.transpose(-2, -1))
-        # v2
-        # cosine attention
-        # attn = (F.normalize(q, dim=-1) @
-        #         F.normalize(k, dim=-1).transpose(-2, -1))
-        # logit_scale = torch.clamp(
-        #     self.logit_scale, max=torch.log(torch.tensor(1. / 0.01).to("cuda"))).exp()
-        # attn = attn * logit_scale
-        ###
 
-        # v2
-        # relative_position_bias_table = self.cpb_mlp(
-        #     self.relative_coords_table).view(-1, self.num_heads)
-        ###
-
-        # v1
         relative_position_bias = self.relative_position_bias_table[self.relative_position_index.view(-1)].view(
             # Wh*Ww,Wh*Ww,nH
             self.window_size[0] * self.window_size[1], self.window_size[0] * self.window_size[1], -1)
-        # v2
-        # relative_position_bias = relative_position_bias_table[self.relative_position_index.view(-1)].view(
-        #     # Wh*Ww,Wh*Ww,nH
-        #     self.window_size[0] * self.window_size[1], self.window_size[0] * self.window_size[1], -1)
         relative_position_bias = relative_position_bias.permute(
             2, 0, 1).contiguous()  # nH, Wh*Ww, Wh*Ww
         attn = attn + relative_position_bias.unsqueeze(0)
 
         if mask is not None:
             nW = mask.shape[0]
-            # print("input mask: ", mask.size())
-            # print("reshape mask: ", mask.unsqueeze(1).unsqueeze(0).size())
-            # print("attention: ", attn.view(
-            #     B_ // nW, nW, self.num_heads, N, N).size())
             attn = attn.view(B_ // nW, nW, self.num_heads, N,
                              N) + mask.unsqueeze(1).unsqueeze(0)
             attn = attn.view(-1, self.num_heads, N, N)
@@ -331,11 +199,6 @@ class WindowAttention(nn.Module):
 
         attn = self.attn_drop(attn)
         x = (attn @ v).transpose(1, 2).reshape(B_, N, C)
-        # v2
-        # x = self.proj(x)
-        # x = self.proj_drop(x)
-        ###
-
         return x, attn
 
     def extra_repr(self) -> str:
@@ -475,16 +338,18 @@ class StyleSwinTransformerBlock(nn.Module):
         q1_windows, k1_windows, v1_windows = self.get_window_qkv(qkv_1)
         q2_windows, k2_windows, v2_windows = self.get_window_qkv(qkv_2)
 
+        # attention mask from user mask
         if mask1 is not None:
-            mask1 = mask1.unsqueeze(0).unsqueeze(-1)
+            mask1 = mask1.unsqueeze(0).unsqueeze(0)
+            mask1 = F.interpolate(mask1, (H, W), mode='bilinear')
+            mask1 = mask1.squeeze(0).unsqueeze(-1)
             window_mask1 = window_partition(mask1, self.window_size)
             window_mask1 = window_mask1.view(-1,
                                              self.window_size * self.window_size)
             attn_mask1 = window_mask1.unsqueeze(1) - window_mask1.unsqueeze(2)
             attn_mask1 = attn_mask1.masked_fill(
                 attn_mask1 != 0, float(-100.0)).masked_fill(attn_mask1 == 0, float(0.0))
-            self.mask1 = attn_mask1
-            print(attn_mask1)
+            self.attn_mask1 = attn_mask1
 
         x1, attn1 = self.attn[0](
             q1_windows, k1_windows, v1_windows, self.attn_mask1)
@@ -510,15 +375,6 @@ class StyleSwinTransformerBlock(nn.Module):
         x = shortcut + x
         x = x + self.mlp(self.norm2(x.transpose(-1, -2),
                          style).transpose(-1, -2))
-
-        # v2
-        # x = shortcut + \
-        #     self.drop_path(self.norm1(
-        #         x.transpose(-1, -2), style).transpose(-1, -2))
-        # x = x + \
-        #     self.drop_path(self.norm2(
-        #         self.mlp(x).transpose(-1, -2), style).transpose(-1, -2))
-        ###
 
         if return_attn:
             return x, attn1, attn2
@@ -834,22 +690,6 @@ class Generator(nn.Module):
 
             styles = torch.cat(style_t, dim=0)
 
-        # if len(styles) < 2:
-        #     inject_index = self.n_latent
-        #
-        #     if styles[0].ndim < 3:
-        #         latent = styles[0].unsqueeze(1).repeat(1, inject_index, 1)
-        #     else:
-        #         latent = styles[0]
-        # else:
-        #     if inject_index is None:
-        #         inject_index = random.randint(1, self.n_latent - 1)
-        #
-        #     latent = styles[0].unsqueeze(1).repeat(1, inject_index, 1)
-        #     latent2 = styles[1].unsqueeze(1).repeat(1, self.n_latent - inject_index, 1)
-        #
-        #     latent = torch.cat([latent, latent2], dim=1)
-
         if styles.ndim < 3:
             latent = styles.unsqueeze(1).repeat(1, inject_index, 1)
         else:
@@ -866,14 +706,12 @@ class Generator(nn.Module):
         count = 0
         skip = None
         for layer, to_rgb in zip(self.layers, self.to_rgbs):
-            if return_attention:
-                x, attn = layer(
-                    x, latent[:, count, :], latent[:, count+1, :], return_attn=True, mask1=mask1)
+            x, attn = layer(
+                x, latent[:, count, :], latent[:, count+1, :], return_attn=return_attention, mask1=mask1)
 
+            if return_attention:
                 attention.append(attn)
-            else:
-                x, _ = layer(
-                    x, latent[:, count, :], latent[:, count+1, :], return_attn=False)
+
             if return_features:
                 features.append(x)
 

models/generatorv2.py
@@ -176,6 +176,29 @@ def apply_rotary_emb(
     return xq_out.type_as(xq).to(xq.device), xk_out.type_as(xk).to(xk.device)
 
 
+class SwiGLUFFN(nn.Module):
+    def __init__(
+        self,
+        in_features,
+        hidden_features=None,
+        out_features=None,
+        act_layer=None,
+        drop=0.0,
+        bias=True,
+    ):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        self.w12 = nn.Linear(in_features, 2 * hidden_features, bias=bias)
+        self.w3 = nn.Linear(hidden_features, out_features, bias=bias)
+
+    def forward(self, x):
+        x12 = self.w12(x)
+        x1, x2 = x12.chunk(2, dim=-1)
+        hidden = F.silu(x1) * x2
+        return self.w3(hidden)
+
+
 class WindowAttention(nn.Module):
     r""" Window based multi-head self attention (W-MSA) module with relative position bias.
     It supports both of shifted and non-shifted window.
@@ -189,14 +212,11 @@ class WindowAttention(nn.Module):
         attn_drop (float, optional): Dropout ratio of attention weight. Default: 0.0
     """
 
-    def __init__(self, dim, window_size, num_heads, qk_scale=None, attn_drop=0., proj_drop=0., pretrained_window_size=[0, 0]):
+    def __init__(self, dim, window_size, num_heads, rope_theta=10.0, qk_scale=None, attn_drop=0., proj_drop=0., pretrained_window_size=[0, 0]):
 
         super().__init__()
         self.dim = dim
         self.window_size = window_size  # Wh, Ww
-        # v2
-        self.pretrained_window_size = pretrained_window_size
-        ###
         self.num_heads = num_heads
         head_dim = dim // num_heads
         self.head_dim = head_dim
@@ -207,68 +227,23 @@ class WindowAttention(nn.Module):
             torch.log(10 * torch.ones((num_heads, 1, 1))), requires_grad=True)
         ###
 
-        # v2
-        # mlp to generate continuous relative position bias
-        self.cpb_mlp = nn.Sequential(nn.Linear(2, 512, bias=True),
-                                     nn.ReLU(inplace=True),
-                                     nn.Linear(512, num_heads, bias=False))
+        # rope vit
+        self.rope_mixed = True
+        t_x, t_y = init_t_xy(
+            end_x=self.window_size[1], end_y=self.window_size[0])
+        self.register_buffer('rope_t_x', t_x)
+        self.register_buffer('rope_t_y', t_y)
 
-        # get relative_coords_table
-        relative_coords_h = torch.arange(
-            -(self.window_size[0] - 1), self.window_size[0], dtype=torch.float32)
-        relative_coords_w = torch.arange(
-            -(self.window_size[1] - 1), self.window_size[1], dtype=torch.float32)
-        relative_coords_table = torch.stack(
-            torch.meshgrid([relative_coords_h,
-                            # 1, 2*Wh-1, 2*Ww-1, 2
-                            relative_coords_w])).permute(1, 2, 0).contiguous().unsqueeze(0)
-        if pretrained_window_size[0] > 0:
-            relative_coords_table[:, :, :,
-                                  0] /= (pretrained_window_size[0] - 1)
-            relative_coords_table[:, :, :,
-                                  1] /= (pretrained_window_size[1] - 1)
-        else:
-            relative_coords_table[:, :, :, 0] /= (self.window_size[0] - 1)
-            relative_coords_table[:, :, :, 1] /= (self.window_size[1] - 1)
-        relative_coords_table *= 8  # normalize to -8, 8
-        relative_coords_table = torch.sign(relative_coords_table) * torch.log2(
-            torch.abs(relative_coords_table) + 1.0) / np.log2(8)
-
-        self.register_buffer("relative_coords_table", relative_coords_table)
+        freqs = init_random_2d_freqs(
+            head_dim=self.dim // self.num_heads, num_heads=self.num_heads, theta=rope_theta,
+            rotate=self.rope_mixed
+        )
+        self.rope_freqs = nn.Parameter(freqs, requires_grad=True)
         ###
-
-        # define a parameter table of relative position bias
-        # in swin-t there's no use
-        self.relative_position_bias_table = nn.Parameter(
-            # 2*Wh-1 * 2*Ww-1, nH
-            torch.zeros((2 * window_size[0] - 1) * (2 * window_size[1] - 1), num_heads))
-
-        # get pair-wise relative position index for each token inside the window
-        coords_h = torch.arange(self.window_size[0])
-        coords_w = torch.arange(self.window_size[1])
-        coords = torch.stack(torch.meshgrid([coords_h, coords_w]))  # 2, Wh, Ww
-        coords_flatten = torch.flatten(coords, 1)  # 2, Wh*Ww
-        relative_coords = coords_flatten[:, :, None] - \
-            coords_flatten[:, None, :]  # 2, Wh*Ww, Wh*Ww
-        relative_coords = relative_coords.permute(
-            1, 2, 0).contiguous()  # Wh*Ww, Wh*Ww, 2
-        relative_coords[:, :, 0] += self.window_size[0] - \
-            1  # shift to start from 0
-        relative_coords[:, :, 1] += self.window_size[1] - 1
-        relative_coords[:, :, 0] *= 2 * self.window_size[1] - 1
-        relative_position_index = relative_coords.sum(-1)  # Wh*Ww, Wh*Ww
-        self.register_buffer("relative_position_index",
-                             relative_position_index)
-        trunc_normal_(self.relative_position_bias_table, std=.02)
 
         self.attn_drop = nn.Dropout(attn_drop)
 
         self.softmax = nn.Softmax(dim=-1)
-
-        # v2
-        self.proj = nn.Linear(dim, dim)
-        self.proj_drop = nn.Dropout(proj_drop)
-        ###
 
     def forward(self, q, k, v, mask=None):
         """
@@ -288,8 +263,11 @@ class WindowAttention(nn.Module):
 
         q = q * self.scale
 
-        # v1
-        # attn = (q @ k.transpose(-2, -1))
+        # rope vit
+        freqs_cis = compute_cis(self.rope_freqs, self.rope_t_x, self.rope_t_y)
+        q, k = apply_rotary_emb(q, k, freqs_cis)
+        ###
+
         # v2
         # cosine attention
         attn = (F.normalize(q, dim=-1) @
@@ -298,23 +276,6 @@ class WindowAttention(nn.Module):
             self.logit_scale, max=torch.log(torch.tensor(1. / 0.01).to("cuda"))).exp()
         attn = attn * logit_scale
         ###
-
-        # v2
-        relative_position_bias_table = self.cpb_mlp(
-            self.relative_coords_table).view(-1, self.num_heads)
-        ###
-
-        # v1
-        # relative_position_bias = self.relative_position_bias_table[self.relative_position_index.view(-1)].view(
-        #     # Wh*Ww,Wh*Ww,nH
-        #     self.window_size[0] * self.window_size[1], self.window_size[0] * self.window_size[1], -1)
-        # v2
-        relative_position_bias = relative_position_bias_table[self.relative_position_index.view(-1)].view(
-            # Wh*Ww,Wh*Ww,nH
-            self.window_size[0] * self.window_size[1], self.window_size[0] * self.window_size[1], -1)
-        relative_position_bias = relative_position_bias.permute(
-            2, 0, 1).contiguous()  # nH, Wh*Ww, Wh*Ww
-        attn = attn + relative_position_bias.unsqueeze(0)
 
         if mask is not None:
             nW = mask.shape[0]
@@ -327,10 +288,6 @@ class WindowAttention(nn.Module):
 
         attn = self.attn_drop(attn)
         x = (attn @ v).transpose(1, 2).reshape(B_, N, C)
-        # v2
-        x = self.proj(x)
-        x = self.proj_drop(x)
-        ###
 
         return x, attn
 
@@ -447,8 +404,16 @@ class StyleSwinTransformerBlock(nn.Module):
             drop_path) if drop_path > 0. else nn.Identity()
         self.norm2 = AdaptiveInstanceNorm(dim, style_dim)
         mlp_hidden_dim = int(dim * mlp_ratio)
-        self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim,
-                       act_layer=act_layer, drop=drop)
+
+        # OG
+        # self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim,
+        #                act_layer=act_layer, drop=drop)
+        ###
+
+        # swiglu ffn
+        self.mlp = SwiGLUFFN(in_features=dim, hidden_features=mlp_hidden_dim,
+                             act_layer=act_layer, drop=drop)
+        ###
 
     def forward(self, x, style, return_attn=False, mask1=None):
         H, W = self.input_resolution
@@ -471,6 +436,7 @@ class StyleSwinTransformerBlock(nn.Module):
         q1_windows, k1_windows, v1_windows = self.get_window_qkv(qkv_1)
         q2_windows, k2_windows, v2_windows = self.get_window_qkv(qkv_2)
 
+        # mask attention one for restrict view
         if mask1 is not None:
             mask1 = mask1.unsqueeze(0).unsqueeze(-1)
             window_mask1 = window_partition(mask1, self.window_size)
@@ -479,8 +445,7 @@ class StyleSwinTransformerBlock(nn.Module):
             attn_mask1 = window_mask1.unsqueeze(1) - window_mask1.unsqueeze(2)
             attn_mask1 = attn_mask1.masked_fill(
                 attn_mask1 != 0, float(-100.0)).masked_fill(attn_mask1 == 0, float(0.0))
-            self.mask1 = attn_mask1
-            print(attn_mask1)
+            self.attn_mask1 = attn_mask1
 
         x1, attn1 = self.attn[0](
             q1_windows, k1_windows, v1_windows, self.attn_mask1)
@@ -503,18 +468,9 @@ class StyleSwinTransformerBlock(nn.Module):
         x = self.proj(x)
 
         # FFN
-        # x = shortcut + x
-        # x = x + self.mlp(self.norm2(x.transpose(-1, -2),
-        #                  style).transpose(-1, -2))
-
-        # v2
-        x = shortcut + \
-            self.drop_path(self.norm1(
-                x.transpose(-1, -2), style).transpose(-1, -2))
-        x = x + \
-            self.drop_path(self.norm2(
-                self.mlp(x).transpose(-1, -2), style).transpose(-1, -2))
-        ###
+        x = shortcut + x
+        x = x + self.mlp(self.norm2(x.transpose(-1, -2),
+                         style).transpose(-1, -2))
 
         if return_attn:
             return x, attn1, attn2
@@ -597,7 +553,7 @@ class StyleBasicLayer(nn.Module):
         else:
             self.upsample = None
 
-    def forward(self, x, latent1, latent2, return_attn=False, mask1=None):
+    def forward(self, x, latent1, latent2, return_attn=True, mask1=None):
         if self.use_checkpoint:
             x = checkpoint.checkpoint(self.blocks[0], x, latent1)
             x = checkpoint.checkpoint(self.blocks[1], x, latent2)

train_styleswin.py
@@ -25,7 +25,7 @@ import time
 
 from dataset.dataset import MultiResolutionDataset
 from models.discriminator import Discriminator
-from models.generator import Generator
+from models.generatorv2 import Generator
 from utils import fid_score
 from utils.CRDiffAug import CR_DiffAug
 from utils.distributed import get_rank, reduce_loss_dict, synchronize
@@ -60,7 +60,8 @@ def sample_data(loader):
 
 
 def d_logistic_loss(real_pred, fake_pred):
-    assert type(real_pred) == type(fake_pred), "real_pred must be the same type as fake_pred"
+    assert type(real_pred) == type(
+        fake_pred), "real_pred must be the same type as fake_pred"
     real_loss = F.softplus(-real_pred)
     fake_loss = F.softplus(fake_pred)
     return real_loss.mean() + fake_loss.mean()
@@ -70,7 +71,8 @@ def d_r1_loss(real_pred, real_img):
     grad_real, = autograd.grad(
         outputs=real_pred.sum(), inputs=real_img, create_graph=True
     )
-    grad_penalty = grad_real.pow(2).reshape(grad_real.shape[0], -1).sum(1).mean()
+    grad_penalty = grad_real.pow(2).reshape(
+        grad_real.shape[0], -1).sum(1).mean()
     return grad_penalty
 
 
@@ -88,10 +90,11 @@ def set_grad_none(model, targets):
 def tensor_transform_reverse(image):
     assert image.dim() == 4
     moco_input = torch.zeros(image.size()).type_as(image)
-    moco_input[:,0,:,:] = image[:,0,:,:] * 0.229 + 0.485
-    moco_input[:,1,:,:] = image[:,1,:,:] * 0.224 + 0.456
-    moco_input[:,2,:,:] = image[:,2,:,:] * 0.225 + 0.406
+    moco_input[:, 0, :, :] = image[:, 0, :, :] * 0.229 + 0.485
+    moco_input[:, 1, :, :] = image[:, 1, :, :] * 0.224 + 0.456
+    moco_input[:, 2, :, :] = image[:, 2, :, :] * 0.225 + 0.406
     return moco_input
+
 
 def train(args, loader, generator, discriminator, g_optim, d_optim, g_ema, device):
     if get_rank() == 0 and args.tf_log:
@@ -141,11 +144,11 @@ def train(args, loader, generator, discriminator, g_optim, d_optim, g_ema, devic
         requires_grad(discriminator, True)
         noise = torch.randn((args.batch, 512)).cuda()
 
-        fake_img, _ = generator(noise)
+        fake_img, _, _ = generator(noise, input_is_latent=True)
         fake_pred = discriminator(fake_img)
         real_pred = discriminator(real_img)
         d_loss = d_logistic_loss(real_pred, fake_pred) * args.gan_weight
-        
+
         if args.bcr:
             real_img_cr_aug = CR_DiffAug(real_img)
             fake_img_cr_aug = CR_DiffAug(fake_img)
@@ -169,7 +172,8 @@ def train(args, loader, generator, discriminator, g_optim, d_optim, g_ema, devic
             r1_loss = d_r1_loss(real_pred, real_img)
 
             discriminator.zero_grad()
-            (args.gan_weight * (args.r1 / 2 * r1_loss * args.d_reg_every + 0 * real_pred[0])).backward()
+            (args.gan_weight * (args.r1 / 2 * r1_loss *
+             args.d_reg_every + 0 * real_pred[0])).backward()
 
             d_optim.step()
 
@@ -187,9 +191,9 @@ def train(args, loader, generator, discriminator, g_optim, d_optim, g_ema, devic
             real_img = real_img.to(device)
 
         noise = torch.randn((args.batch, 512)).cuda()
-        fake_img, _ = generator(noise)
+        fake_img, _, _ = generator(noise, input_is_latent=True)
         fake_pred = discriminator(fake_img)
-        g_loss = g_nonsaturating_loss(fake_pred)* args.gan_weight
+        g_loss = g_nonsaturating_loss(fake_pred) * args.gan_weight
 
         loss_dict["g"] = g_loss
         generator.zero_grad()
@@ -206,8 +210,9 @@ def train(args, loader, generator, discriminator, g_optim, d_optim, g_ema, devic
 
         if args.lr_decay and i > args.lr_decay_start_steps:
             args.G_lr -= lr_decay_per_step
-            args.D_lr = args.G_lr * 4 if args.ttur else (args.D_lr - lr_decay_per_step)
-        
+            args.D_lr = args.G_lr * \
+                4 if args.ttur else (args.D_lr - lr_decay_per_step)
+
             for param_group in d_optim.param_groups:
                 param_group['lr'] = args.D_lr
             for param_group in g_optim.param_groups:
@@ -220,17 +225,20 @@ def train(args, loader, generator, discriminator, g_optim, d_optim, g_ema, devic
                     'd_loss': d_loss_val,
                     'g_loss': g_loss_val,
                     'r1_val': r1_val,
-                    }
+                }
                 if wandb and args.wandb:
                     wandb.log(vis_loss, step=i)
                 iters_time = time.time() - end
                 end = time.time()
                 if args.lr_decay:
-                    print("Iters: {}\tTime: {:.4f}\tD_loss: {:.4f}\tG_loss: {:.4f}\tR1: {:.4f}\tG_lr: {:e}\tD_lr: {:e}".format(i, iters_time, d_loss_val, g_loss_val, r1_val, args.G_lr, args.D_lr))
+                    print("Iters: {}\tTime: {:.4f}\tD_loss: {:.4f}\tG_loss: {:.4f}\tR1: {:.4f}\tG_lr: {:e}\tD_lr: {:e}".format(
+                        i, iters_time, d_loss_val, g_loss_val, r1_val, args.G_lr, args.D_lr))
                 else:
-                    print("Iters: {}\tTime: {:.4f}\tD_loss: {:.4f}\tG_loss: {:.4f}\tR1: {:.4f}".format(i, iters_time, d_loss_val, g_loss_val, r1_val))
+                    print("Iters: {}\tTime: {:.4f}\tD_loss: {:.4f}\tG_loss: {:.4f}\tR1: {:.4f}".format(
+                        i, iters_time, d_loss_val, g_loss_val, r1_val))
                 if args.tf_log:
-                    vis.plot_dict(vis_loss, step=(i * args.batch * int(os.environ["WORLD_SIZE"])))
+                    vis.plot_dict(vis_loss, step=(
+                        i * args.batch * int(os.environ["WORLD_SIZE"])))
 
             if i != 0 and i % args.eval_freq == 0:
                 torch.save(
@@ -244,15 +252,17 @@ def train(args, loader, generator, discriminator, g_optim, d_optim, g_ema, devic
                     },
                     args.checkpoint_path + f"/{str(i).zfill(6)}.pt",
                 )
-                
+
                 print("=> Evaluation ...")
                 g_ema.eval()
-                fid1 = evaluation(g_ema, args, i * args.batch * int(os.environ["WORLD_SIZE"]))
+                fid1 = evaluation(g_ema, args, i * args.batch *
+                                  int(os.environ["WORLD_SIZE"]))
                 fid_dict = {'fid1': fid1}
                 if wandb and args.wandb:
                     wandb.log({'fid': fid1}, step=i)
                 if args.tf_log:
-                    vis.plot_dict(fid_dict, step=(i * args.batch * int(os.environ["WORLD_SIZE"])))
+                    vis.plot_dict(fid_dict, step=(
+                        i * args.batch * int(os.environ["WORLD_SIZE"])))
 
             if i % args.save_freq == 0:
                 torch.save(
@@ -275,7 +285,7 @@ def evaluation(generator, args, steps):
         with torch.no_grad():
             noise = torch.randn((args.val_batch_size, 512)).cuda()
 
-            out_sample, _ = generator(noise)
+            out_sample, _, _ = generator(noise, input_is_latent=True)
             out_sample = tensor_transform_reverse(out_sample)
 
             if not os.path.exists(os.path.join(args.sample_path, "eval_{}".format(str(steps)))):
@@ -290,10 +300,11 @@ def evaluation(generator, args, steps):
                     nrow=1,
                     padding=0,
                     normalize=True,
-                    range=(0, 1),
+                    # range=(0, 1),
+                    value_range=(0, 1),
                 )
                 cnt += 1
-    
+
     gt_path = args.eval_gt_path
     device = torch.device('cuda:0')
     fid = fid_score.calculate_fid_given_paths([os.path.join(args.sample_path, "eval_{}".format(
@@ -309,7 +320,8 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--path", type=str, default=None, help="Path of training data")
+    parser.add_argument("--path", type=str, default=None,
+                        help="Path of training data")
     parser.add_argument("--iter", type=int, default=800000)
     parser.add_argument("--batch", type=int, default=4)
     parser.add_argument("--size", type=int, default=256)
@@ -321,41 +333,69 @@ if __name__ == "__main__":
     parser.add_argument("--D_lr", type=float, default=0.0002)
     parser.add_argument("--beta1", type=float, default=0.0)
     parser.add_argument("--beta2", type=float, default=0.99)
-    parser.add_argument("--start_dim", type=int, default=512, help="Start dim of generator input dim")
+    parser.add_argument("--start_dim", type=int, default=512,
+                        help="Start dim of generator input dim")
     parser.add_argument("--D_channel_multiplier", type=int, default=2)
     parser.add_argument("--G_channel_multiplier", type=int, default=1)
     parser.add_argument("--local_rank", type=int, default=0)
     parser.add_argument("--print_freq", type=int, default=1000)
     parser.add_argument("--save_freq", type=int, default=20000)
     parser.add_argument("--eval_freq", type=int, default=50000)
-    parser.add_argument('--workers', default=8, type=int, help='Number of workers')
+    parser.add_argument('--workers', default=0, type=int,
+                        help='Number of workers')
 
-    parser.add_argument('--checkpoint_path', default='/tmp', type=str, help='Save checkpoints')
-    parser.add_argument('--sample_path', default='/tmp', type=str, help='Save sample')
-    parser.add_argument('--start_iter', default=0, type=int, help='Start iter number')
-    parser.add_argument('--tf_log', action="store_true", help='If we use tensorboard file')
-    parser.add_argument('--gan_weight', default=1, type=float, help='Gan loss weight')
-    parser.add_argument('--val_num_batches', default=1250, type=int, help='Num of batches will be generated during evalution')
-    parser.add_argument('--val_batch_size', default=4, type=int, help='Batch size during evalution')
-    parser.add_argument('--D_sn', action="store_true", help='If we use spectral norm in D')
-    parser.add_argument('--ttur', action="store_true", help='If we use TTUR during training')
-    parser.add_argument('--eval', action="store_true", help='Only do evaluation')
-    parser.add_argument("--eval_iters", type=int, default=0, help="Iters of evaluation ckpt")
-    parser.add_argument('--eval_gt_path', default='/tmp', type=str, help='Path to ground truth images to evaluate FID score')
-    parser.add_argument('--mlp_ratio', default=4, type=int, help='MLP ratio in swin')
-    parser.add_argument("--lr_mlp", default=0.01, type=float, help='Lr mul for 8 * fc')
-    parser.add_argument("--bcr", action="store_true", help='If we add bcr during training')
-    parser.add_argument("--bcr_fake_lambda", default=10, type=float, help='Bcr weight for fake data')
-    parser.add_argument("--bcr_real_lambda", default=10, type=float, help='Bcr weight for real data')
-    parser.add_argument("--enable_full_resolution", default=8, type=int, help='Enable full resolution attention index')
-    parser.add_argument("--auto_resume", action="store_true", help="Auto resume from checkpoint")
-    parser.add_argument("--lmdb", action="store_true", help='Whether to use lmdb datasets')
-    parser.add_argument("--use_checkpoint", action="store_true", help='Whether to use checkpoint')
-    parser.add_argument("--use_flip", action="store_true", help='Whether to use random flip in training')
-    parser.add_argument("--wandb", action="store_true", help='Whether to use wandb record training')
-    parser.add_argument("--project_name", type=str, default='StyleSwin', help='Project name')
-    parser.add_argument("--lr_decay", action="store_true", help='Whether to use lr decay')
-    parser.add_argument("--lr_decay_start_steps", default=800000, type=int, help='Steps to start lr decay')
+    parser.add_argument('--checkpoint_path', default='/tmp',
+                        type=str, help='Save checkpoints')
+    parser.add_argument('--sample_path', default='/tmp',
+                        type=str, help='Save sample')
+    parser.add_argument('--start_iter', default=0,
+                        type=int, help='Start iter number')
+    parser.add_argument('--tf_log', action="store_true",
+                        help='If we use tensorboard file')
+    parser.add_argument('--gan_weight', default=1,
+                        type=float, help='Gan loss weight')
+    parser.add_argument('--val_num_batches', default=1250, type=int,
+                        help='Num of batches will be generated during evalution')
+    parser.add_argument('--val_batch_size', default=4,
+                        type=int, help='Batch size during evalution')
+    parser.add_argument('--D_sn', action="store_true",
+                        help='If we use spectral norm in D')
+    parser.add_argument('--ttur', action="store_true",
+                        help='If we use TTUR during training')
+    parser.add_argument('--eval', action="store_true",
+                        help='Only do evaluation')
+    parser.add_argument("--eval_iters", type=int, default=0,
+                        help="Iters of evaluation ckpt")
+    parser.add_argument('--eval_gt_path', default='/tmp', type=str,
+                        help='Path to ground truth images to evaluate FID score')
+    parser.add_argument('--mlp_ratio', default=4,
+                        type=int, help='MLP ratio in swin')
+    parser.add_argument("--lr_mlp", default=0.01,
+                        type=float, help='Lr mul for 8 * fc')
+    parser.add_argument("--bcr", action="store_true",
+                        help='If we add bcr during training')
+    parser.add_argument("--bcr_fake_lambda", default=10,
+                        type=float, help='Bcr weight for fake data')
+    parser.add_argument("--bcr_real_lambda", default=10,
+                        type=float, help='Bcr weight for real data')
+    parser.add_argument("--enable_full_resolution", default=8,
+                        type=int, help='Enable full resolution attention index')
+    parser.add_argument("--auto_resume", action="store_true",
+                        help="Auto resume from checkpoint")
+    parser.add_argument("--lmdb", action="store_true",
+                        help='Whether to use lmdb datasets')
+    parser.add_argument("--use_checkpoint", action="store_true",
+                        help='Whether to use checkpoint')
+    parser.add_argument("--use_flip", action="store_true",
+                        help='Whether to use random flip in training')
+    parser.add_argument("--wandb", action="store_true",
+                        help='Whether to use wandb record training')
+    parser.add_argument("--project_name", type=str,
+                        default='StyleSwin', help='Project name')
+    parser.add_argument("--lr_decay", action="store_true",
+                        help='Whether to use lr decay')
+    parser.add_argument("--lr_decay_start_steps", default=800000,
+                        type=int, help='Steps to start lr decay')
 
     args = parser.parse_args()
 
@@ -363,12 +403,13 @@ if __name__ == "__main__":
     args.distributed = n_gpu > 1
 
     args.latent = 4096
-    args.n_mlp = 8 
+    args.n_mlp = 8
     args.g_reg_every = 10000000    # We do not apply regularization on G
 
     if args.distributed:
         torch.cuda.set_device(args.local_rank)
-        torch.distributed.init_process_group(backend="nccl", init_method="env://", timeout=timedelta(0, 18000))
+        torch.distributed.init_process_group(
+            backend="nccl", init_method="env://", timeout=timedelta(0, 18000))
         synchronize()
 
     if args.distributed and get_rank() != 0:
@@ -385,7 +426,8 @@ if __name__ == "__main__":
         args.size, args.style_dim, args.n_mlp, channel_multiplier=args.G_channel_multiplier, lr_mlp=args.lr_mlp,
         enable_full_resolution=args.enable_full_resolution, use_checkpoint=args.use_checkpoint
     ).to(device)
-    discriminator = Discriminator(args.size, channel_multiplier=args.D_channel_multiplier, sn=args.D_sn).to(device)
+    discriminator = Discriminator(
+        args.size, channel_multiplier=args.D_channel_multiplier, sn=args.D_sn).to(device)
     g_ema = Generator(
         args.size, args.style_dim, args.n_mlp, channel_multiplier=args.G_channel_multiplier, lr_mlp=args.lr_mlp,
         enable_full_resolution=args.enable_full_resolution, use_checkpoint=args.use_checkpoint
@@ -406,8 +448,8 @@ if __name__ == "__main__":
         except:
             pass
 
-        generator.load_state_dict(ckpt["g"])
-        g_ema.load_state_dict(ckpt["g_ema"])
+        generator.load_state_dict(ckpt["g"], strict=False)
+        g_ema.load_state_dict(ckpt["g_ema"], strict=False)
         try:
             discriminator.load_state_dict(ckpt["d"])
         except:
@@ -434,7 +476,7 @@ if __name__ == "__main__":
             output_device=args.local_rank,
             broadcast_buffers=False,
         )
-    
+
     g_optim = optim.Adam(
         generator.parameters(),
         lr=args.G_lr * g_reg_ratio if not args.ttur else args.D_lr / 4 * g_reg_ratio,
@@ -461,11 +503,13 @@ if __name__ == "__main__":
     if args.eval:
         if get_rank() == 0:
             g_ema.eval()
-            evaluation(g_ema, args, (args.eval_iters * args.batch * int(os.environ["WORLD_SIZE"])))
+            evaluation(g_ema, args, (args.eval_iters *
+                       args.batch * int(os.environ["WORLD_SIZE"])))
             sys.exit(0)
         sys.exit(0)
 
-    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    normalize = transforms.Normalize(
+        mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     if args.use_flip:
         transform = transforms.Compose(
             [
@@ -488,16 +532,18 @@ if __name__ == "__main__":
         dataset = MultiResolutionDataset(args.path, transform, args.size)
     else:
         dataset = datasets.ImageFolder(root=args.path, transform=transform)
-                
+
     loader = data.DataLoader(
         dataset,
         batch_size=args.batch,
         num_workers=args.workers,
-        sampler=data_sampler(dataset, shuffle=True, distributed=args.distributed),
+        sampler=data_sampler(dataset, shuffle=True,
+                             distributed=args.distributed),
         drop_last=True,
     )
 
     if get_rank() == 0 and wandb is not None and args.wandb:
         wandb.init(project=args.project_name)
 
-    train(args, loader, generator, discriminator, g_optim, d_optim, g_ema, device)
+    train(args, loader, generator, discriminator,
+          g_optim, d_optim, g_ema, device)